ad977f7486fd6791abaece9fac8112a7d3395e07
yufa18 <muhammadyufa18@gmail.com>
2022-12-15 13:05:08 +0700

    upload front-end

---
+++ js/main.js
@@ -0,0 +1,113 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+    });
+
+    // Login registration
+    $('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+        var $this = $(this),
+            label = $this.prev('label');
+      
+            if (e.type === 'keyup') {
+                  if ($this.val() === '') {
+                label.removeClass('active highlight');
+              } else {
+                label.addClass('active highlight');
+              }
+          } else if (e.type === 'blur') {
+              if( $this.val() === '' ) {
+                  label.removeClass('active highlight'); 
+                  } else {
+                  label.removeClass('highlight');   
+                  }   
+          } else if (e.type === 'focus') {
+            
+            if( $this.val() === '' ) {
+                  label.removeClass('highlight'); 
+                  } 
+            else if( $this.val() !== '' ) {
+                  label.addClass('highlight');
+                  }
+          }
+      
+      });
+      
+      $('.tab a').on('click', function (e) {
+        
+        e.preventDefault();
+        
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+        
+        target = $(this).attr('href');
+      
+        $('.tab-content > div').not(target).hide();
+        
+        $(target).fadeIn(600);
+        
+      });
+    
+})(jQuery);
+
